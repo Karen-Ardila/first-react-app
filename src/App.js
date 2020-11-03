3edaa128bdb16ea1133b34a429bdfdff0e0d4148
Karen Ardila <karen.ardila1893@gmail.com>
2020-11-02 23:30:42 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Dojo</h1>
+      <h3>Things to do:</h3>
+      <ul>
+        <li>Learn react</li>
+        <li>Travel the world</li>
+        <li>Love on my doggies</li>
+        <li>Byedon 2020</li>
+      </ul>
+    </div>
+    
+  );
+}
+
+export default App;
