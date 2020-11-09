79baa3bd2e2e877ad80b0573a95f90bca47988bd
Karen Ardila <karen.ardila1893@gmail.com>
2020-11-08 20:27:03 -0600

    adding age button logic

--- src/components/personCard.js
+++ src/components/personCard.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
 export default class PersonCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            increaseAge: this.props.age,
+        };
+    }
+
     render() {
         const { firstName, lastName, age, hairColor } = this.props;
         return (
             <div>
                 <h1>{lastName}, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.increaseAge}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={() => this.setState({ increaseAge: this.state.increaseAge + 1 })}>
+                    Birthday!!</button>
             </div>
         )
     }
+
 }
+
